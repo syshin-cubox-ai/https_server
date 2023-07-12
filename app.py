2f1992775f41379f5f054c23d165b2cf0e157333
신석용 <syshin@cubox.ai>
2023-07-12 11:19:24 +0900

    Tell Flask it is Behind a Proxy

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
 
 
 if __name__ == '__main__':
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
     app.run()
