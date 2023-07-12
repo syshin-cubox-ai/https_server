905c798eb29244776aa52e3e0e30ee9d59b50b34
신석용 <syshin@cubox.ai>
2023-07-12 11:27:10 +0900

    Recv a parameter

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
@@ -7,6 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/param', methods=['GET', 'POST'])
+def param():
+    name = request.args.get('name')
+    if name is None:
+        name = request.form.get('name')
+    return render_template('param.html', name=name)
 
 
 if __name__ == '__main__':
